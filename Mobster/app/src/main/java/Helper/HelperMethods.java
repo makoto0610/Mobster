85665c6ed4dd03ac8b82d5bb50a7b0824707f333
cshih299 <cshih299@gmail.com>
2017-02-15 18:00:16 -0500

    Added a helper method to calculate time remaining given two Calendars.

--- Mobster/app/src/main/java/Helper/HelperMethods.java
+++ Mobster/app/src/main/java/Helper/HelperMethods.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.view.View;
 
+import java.util.Calendar;
+
 /**
  * Created by christineshih on 2/6/17.
  */
@@ -28,5 +30,43 @@ public class HelperMethods {
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+    }
+
+
+    /**
+     * Calculates the time left (duration).
+     *
+     * @param start start Calendar
+     * @param end end Calendar
+     * @return long array in the format of [days left, hours left, minutes left]
+     */
+    public static long[] computeDuration(Calendar start, Calendar end) {
+        long[] returned = new long[3];
+
+        long startTime = start.getTimeInMillis();
+        long endTime = end.getTimeInMillis();
+
+        long difference = endTime - startTime;
+
+        final long secondsInMilli = 1000;
+        final long minutesInMilli = secondsInMilli * 60;
+        final long hoursInMilli = minutesInMilli * 60;
+        final long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = difference / daysInMilli;
+        difference = difference % daysInMilli;
+
+        long elapsedHours = difference / hoursInMilli;
+        difference = difference % hoursInMilli;
+
+        long elapsedMinutes = difference / minutesInMilli;
+
+        // just need to do one more time if want to obtain seconds, would need to modify ret value
+
+        returned[0] = elapsedDays;
+        returned[1] = elapsedHours;
+        returned[2] = elapsedMinutes;
+
+        return returned;
     }
 }
